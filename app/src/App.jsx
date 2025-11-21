71adae56afdd01dcadeff8cfa53004d96eb39218
Oswald2809 <oswaldtanlee44@gmail.com>
2025-11-21 15:36:31 +0800

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing";
+import NotFoundPage from "./components/404";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
